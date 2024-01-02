020682eb0af3a35abe8697dae826aa17cb4d1fbe
Cthede11 <147101801+Cthede11@users.noreply.github.com>
2024-01-01 19:54:47 -0600

    index update + js + mobile styling

--- docs/indexScript.js
+++ docs/indexScript.js
@@ -1,24 +1,29 @@
+
+// Simplified expandFunction
 function expandFunction() {
   var x = document.getElementById("expandGallery");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+  x.style.display = (x.style.display === "none") ? "block" : "none";
 }
 
+// Handle JavaScript disabled case
 var expandButton = document.getElementById("expandButton");
 var expandGallery = document.getElementById("expandGallery");
 
 expandGallery.style.display = "none";
-expandButton.addEventListener("click", expandFunction);
+if (expandButton && expandGallery) {
+  
+  expandButton.addEventListener("click", expandFunction);
+}
 
+// Use appropriate variable names
+var projectsButton = document.getElementById("projectsButton");
 
+if (projectsButton) {
+  projectsButton.addEventListener("mouseover", function() {
+      this.classList.add("hover");
+  });
 
-projectsButton.addEventListener("mouseover", function() {
-  this.classList.add("hover");
-});
-
-projectsButton.addEventListener("mouseout", function() {
-  this.classList.remove("hover");
-});
+  projectsButton.addEventListener("mouseout", function() {
+      this.classList.remove("hover");
+  });
+}
