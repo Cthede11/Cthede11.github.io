94e561e02c84a34b42fdf9a6b2570af8b42208ea
Cthede11 <147101801+Cthede11@users.noreply.github.com>
2024-01-03 15:12:27 -0600

    added popup
    for best experience use desktop or laptop

--- docs/scriptLibrary/indexScript.js
+++ docs/scriptLibrary/indexScript.js
@@ -11,3 +11,29 @@ if (projectsButton) {
       this.classList.remove("hover");
   });
 }
+
+// popup on page load
+window.onload = function() {
+  document.getElementById('popup').style.display = 'block';
+}
+
+document.getElementById('closePopup').onclick = function() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+window.onload = function() {
+  // Check if 'visited' flag is in local storage
+  if (!localStorage.getItem('visited')) {
+      // If not, show the popup
+      document.getElementById('popup').style.display = 'block';
+  }
+}
+
+document.getElementById('closePopup').onclick = function() {
+  document.getElementById('popup').style.display = 'none';
+
+  // If the checkbox is checked, set the 'visited' flag in local storage
+  if (document.getElementById('noShow').checked) {
+      localStorage.setItem('visited', 'true');
+  }
+}
