48a1828ae93069e389d3bee10d706e2ab07564c4
Cthede11 <147101801+Cthede11@users.noreply.github.com>
2024-05-07 16:54:31 -0500

    skill bar and gallery update
    added x86 and arm to the skills list
    added more photos to the gallery and the expand button will now change to retract when section is expanded, there is also another retract button at the bottom of the section

--- docs/scriptLibrary/expandButtonScript.js
+++ docs/scriptLibrary/expandButtonScript.js
@@ -1,16 +1,32 @@
-// expandGallery button effects
-function expandFunction() {
-    var x = document.getElementById("expandGallery");
-    x.style.display = (x.style.display === "none") ? "block" : "none";
+window.onload = function() {
+  // expandGallery button effects
+  function expandFunction() {
+      var x = document.getElementById("expandGallery");
+      var expandButton = document.getElementById("expandButton");
+      var retractButton = document.getElementById("retractButton");
+
+      if (x.style.display === "none") {
+          x.style.display = "block";
+          expandButton.innerText = "Retract";
+          retractButton.style.display = "block";
+      } else {
+          x.style.display = "none";
+          expandButton.innerText = "Expand";
+          retractButton.style.display = "none";
+      }
   }
-  
+
   // Handle JavaScript disabled case
   var expandButton = document.getElementById("expandButton");
+  var retractButton = document.getElementById("retractButton");
   var expandGallery = document.getElementById("expandGallery");
-  
+
   expandGallery.style.display = "none";
+  retractButton.style.display = "none";
   if (expandButton && expandGallery) {
-    
-    expandButton.addEventListener("click", expandFunction);
+      expandButton.addEventListener("click", expandFunction);
   }
-  
+  if (retractButton && expandGallery) {
+      retractButton.addEventListener("click", expandFunction);
+  }
+}
